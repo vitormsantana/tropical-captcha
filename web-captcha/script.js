7cada5015ae55f52fb74b3fdd2e80a5c9c4dd64f
vitormsantana <vitormsantana25@gmail.com>
2023-08-24 02:47:43 -0300

    ok

--- web-captcha/script.js
+++ web-captcha/script.js
@@ -1,22 +1,74 @@
-document.addEventListener("DOMContentLoaded", () => {
+// Loading the Captcha Image and Sending Base64 Data: //
+
+document.addEventListener("DOMContentLoaded", async () => {
     const captchaImage = document.getElementById("captcha-image");
-    const captchaInput = document.getElementById("captcha-input");
-    const verifyButton = document.getElementById("verify-button");
-    const resultMessage = document.getElementById("result-message");
+    const predictButton = document.getElementById("predict-button");
+    const resultContainer = document.getElementById("result-container");
 
-    // Load captcha image from your local server
-    captchaImage.src = "http://localhost:8000/captcha2564linesi.PNG"; // Replace with your image file name
+    captchaImage.src = "http://localhost:8000/processed_p5d3tr.PNG"; // Set your image source
 
-    verifyButton.addEventListener("click", () => {
-        const userInput = captchaInput.value;
-        // Use your Keras model to solve the captcha and get the expected solution
-        const expectedSolution = "i"; // Replace with your actual expected solution
+    predictButton.addEventListener("click", async () => {
+        try {
+            const base64Captcha = await getBase64FromImage(captchaImage);
 
-        if (userInput === expectedSolution) {
-            resultMessage.textContent = "Captcha solved correctly!";
-        } else {
-            resultMessage.textContent = "Captcha solution is incorrect.";
+            // Create a JSON object with the base64 data
+            const requestData = {
+                data: base64Captcha
+            };
+
+            // Send the JSON object to your AWS REST API
+            const response = await sendBase64ToAPI(requestData);
+
+            // Display the prediction result
+            const responseBody = await response.json();
+            resultContainer.textContent = "Prediction: " + responseBody.prediction;
+        } catch (error) {
+            console.error("An error occurred:", error);
+            resultContainer.textContent = "Error: Failed to predict";
         }
     });
 });
 
+// Extracting Base64 Data from Image: //
+
+async function getBase64FromImage(imageElement) {
+    return new Promise((resolve, reject) => {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+
+        canvas.width = imageElement.width;
+        canvas.height = imageElement.height;
+
+        context.drawImage(imageElement, 0, 0, canvas.width, canvas.height);
+
+        const base64Data = canvas.toDataURL("image/png").split(',')[1];
+        resolve(base64Data);
+    });
+}
+
+
+// Sending Base64 Data to AWS API://
+
+async function sendBase64ToAPI(data) {
+    const apiUrl = "https://132fpvvrhi.execute-api.sa-east-1.amazonaws.com/test"; // Replace with your AWS API endpoint URL
+
+    try {
+        const response = await fetch(apiUrl, {
+            //mode: 'no-cors',
+			//credentials: 'same-origin',
+			method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.ok) {
+            console.log("Base64 data sent successfully.");
+        } else {
+            console.error("Error sending base64 data.");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
